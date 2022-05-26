d7c50384acdaf2ac04151845a6b185e43f9285c0
Joshua <joshsmayhew@gmail.com>
2022-05-26 21:33:25 +0700

    working on event listener on nav bar headings

--- script.js
+++ script.js
@@ -2,3 +2,20 @@ const h2 = document.createElement("h2");
 h2.textContent = "This content added by Javascript";
 document.querySelector("body").appendChild(h2);
 
+
+
+const link = document.getElementById("home", "mystory", "projects", "contact");
+
+function linkColoChange (event) {
+    if (link.onmouseover) {
+        link.style.color = '#F72585';
+    } else if (link.onmouseout) {
+        link.style.color = '#F72585';
+    }else {
+        link.style.color = '#fff';
+    }
+
+};
+
+link.addEventListener('mouseover', linkColorChange);
+
